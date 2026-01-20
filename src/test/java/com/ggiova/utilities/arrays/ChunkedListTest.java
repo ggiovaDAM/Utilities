8ab9d3d13c7471a8be016b435a75071b8f87d24b
ggiovadam <guido.giovadam@gmail.com>
2026-01-20 14:19:59 +0100

    [chore] Format tests from ChunkedListTest.java

--- src/test/java/com/ggiova/utilities/arrays/ChunkedListTest.java
+++ src/test/java/com/ggiova/utilities/arrays/ChunkedListTest.java
@@ -12,135 +12,88 @@ import static org.junit.jupiter.api.Assertions.*;
  * Test class for {@link ChunkedList}
  */
 public class ChunkedListTest {
-    @Test
-    void length_reading_when_empty() {
-        ChunkedList<Integer> integerChunkedList = new ChunkedList<>();
-        assertEquals(0, integerChunkedList.length());
-    }
-    
-    @Test
-    void chunk_count_when_empty() {
-        ChunkedList<Integer> integerChunkedList = new ChunkedList<>();
-        assertEquals(1, integerChunkedList.chunkCount());
-    }
-    
-    @Test
-    void length_reading_after_adding() {
-        ChunkedList<Integer> integerChunkedList = new ChunkedList<>();
-        integerChunkedList.add(1);
-        assertEquals(1, integerChunkedList.length());
-    }
-    
-    @Test
-    void chunk_count_after_adding() {
-        ChunkedList<Integer> integerChunkedList = new ChunkedList<>();
-        integerChunkedList.add(1);
-        assertEquals(1, integerChunkedList.chunkCount());
-    }
-    
-    @Test
-    void length_reading_after_adding_17_elements() {
-        final int TOTAL_TO_ADD = 17;
-        ChunkedList<Integer> integerChunkedList = new ChunkedList<>();
-        for (int ii = 0; ii < TOTAL_TO_ADD; ii++) {
-            integerChunkedList.add(ii);
-        }
-        assertEquals(TOTAL_TO_ADD, integerChunkedList.length());
-    }
-    
-    @Test
-    void chunk_count_after_adding_17_elements() {
-        final int TOTAL_TO_ADD = 17;
-        ChunkedList<Integer> integerChunkedList = new ChunkedList<>();
-        for (int ii = 0; ii < TOTAL_TO_ADD; ii++) {
-            integerChunkedList.add(ii);
-        }
-        assertEquals(2, integerChunkedList.chunkCount());
-    }
-    
-    @Test
-    void value_test_after_getting_from_index() {
-        final String EXPECTED = "EXPECTED VALUE";
-        ChunkedList<String> stringChunkedList = new ChunkedList<>();
-        stringChunkedList.add(EXPECTED);
-        String result = stringChunkedList.get(0);
-        assertEquals(EXPECTED, result);
-    }
-    
-    @Test
-    void value_test_out_of_bounds() {
-        final int INDEX = 999;
-        ChunkedList<String> stringChunkedList = new ChunkedList<>();
-        assertThrows(IndexOutOfBoundsException.class, () -> stringChunkedList.get(INDEX));
-    }
-    
-    @Test
-    void value_test_out_of_bounds_edge() {
-        final int INDEX = 0;
-        ChunkedList<String> stringChunkedList = new ChunkedList<>();
-        assertThrows(IndexOutOfBoundsException.class, () -> stringChunkedList.get(INDEX));
-    }
-    
-    @Test
-    void value_test_out_of_bounds_negative() {
-        final int INDEX = -10;
-        ChunkedList<String> stringChunkedList = new ChunkedList<>();
-        assertThrows(IndexOutOfBoundsException.class, () -> stringChunkedList.get(INDEX));
-    }
-    
-    @Test
-    void length_reading_after_subtracting_from_index_after_adding() {
-        ChunkedList<Integer> integerChunkedList = new ChunkedList<>();
-        integerChunkedList.add(1);
-        integerChunkedList.remove(0);
-        assertEquals(0, integerChunkedList.length());
-    }
-    
-    @Test
-    void chunk_count_after_subtracting_from_index_after_adding() {
-        ChunkedList<Integer> integerChunkedList = new ChunkedList<>();
-        integerChunkedList.add(1);
-        integerChunkedList.remove(0);
-        assertEquals(1, integerChunkedList.chunkCount());
-    }
-    
-    @Test
-    void length_reading_after_adding_1_after_removing_1() {
-        ChunkedList<Integer> integerChunkedList = new ChunkedList<>();
-        integerChunkedList.add(1);
-        integerChunkedList.remove(0);
-        integerChunkedList.add(2);
-        assertEquals(1, integerChunkedList.length);
-    }
-    
-    @Test
-    void remove_shifts_elements_left() {
-        ChunkedList<Integer> integerChunkedList = new ChunkedList<>();
-        
-        integerChunkedList.add(1);
-        integerChunkedList.add(2);
-        integerChunkedList.add(3);
-        
-        integerChunkedList.remove(1);
-        
-        assertEquals(2, integerChunkedList.length());
-        assertEquals(3, integerChunkedList.get(1));
-    }
-    
-    @Test
-    void remove_element_from_middle_chunk() {
-        ChunkedList<Integer> integerChunkedList = new ChunkedList<>();
-        
-        for (int i = 0; i < 20; i++) {
-            integerChunkedList.add(i);
+    @Nested
+    @DisplayName("Measurements")
+    class MeasurementTests {
+        @Nested
+        @DisplayName("Length")
+        class LengthTests {
+            @Test
+            void length_reading_when_empty() {
+                ChunkedList<Integer> integerChunkedList = new ChunkedList<>();
+                assertEquals(0, integerChunkedList.length());
+            }
+            
+            @Test
+            void length_reading_after_adding() {
+                ChunkedList<Integer> integerChunkedList = new ChunkedList<>();
+                integerChunkedList.add(1);
+                assertEquals(1, integerChunkedList.length());
+            }
+            
+            @Test
+            void length_reading_after_adding_17_elements() {
+                final int TOTAL_TO_ADD = 17;
+                ChunkedList<Integer> integerChunkedList = new ChunkedList<>();
+                for (int ii = 0; ii < TOTAL_TO_ADD; ii++) {
+                    integerChunkedList.add(ii);
+                }
+                assertEquals(TOTAL_TO_ADD, integerChunkedList.length());
+            }
+            
+            @Test
+            void length_reading_after_subtracting_from_index_after_adding() {
+                ChunkedList<Integer> integerChunkedList = new ChunkedList<>();
+                integerChunkedList.add(1);
+                integerChunkedList.remove(0);
+                assertEquals(0, integerChunkedList.length());
+            }
+            
+            @Test
+            void length_reading_after_adding_1_after_removing_1() {
+                ChunkedList<Integer> integerChunkedList = new ChunkedList<>();
+                integerChunkedList.add(1);
+                integerChunkedList.remove(0);
+                integerChunkedList.add(2);
+                assertEquals(1, integerChunkedList.length);
+            }
         }
         
-        integerChunkedList.remove(10);
-        
-        assertEquals(19, integerChunkedList.length());
-        assertEquals(11, integerChunkedList.get(10));
+        @Nested
+        @DisplayName("Chunk count")
+        class ChunkCountTests {
+            @Test
+            void chunk_count_when_empty() {
+                ChunkedList<Integer> integerChunkedList = new ChunkedList<>();
+                assertEquals(1, integerChunkedList.chunkCount());
+            }
+            
+            @Test
+            void chunk_count_after_adding() {
+                ChunkedList<Integer> integerChunkedList = new ChunkedList<>();
+                integerChunkedList.add(1);
+                assertEquals(1, integerChunkedList.chunkCount());
+            }
+            
+            @Test
+            void chunk_count_after_adding_17_elements() {
+                final int TOTAL_TO_ADD = 17;
+                ChunkedList<Integer> integerChunkedList = new ChunkedList<>();
+                for (int ii = 0; ii < TOTAL_TO_ADD; ii++) {
+                    integerChunkedList.add(ii);
+                }
+                assertEquals(2, integerChunkedList.chunkCount());
+            }
+            
+            @Test
+            void chunk_count_after_subtracting_from_index_after_adding() {
+                ChunkedList<Integer> integerChunkedList = new ChunkedList<>();
+                integerChunkedList.add(1);
+                integerChunkedList.remove(0);
+                assertEquals(1, integerChunkedList.chunkCount());
+            }
+        }
     }
-    
     
     @Nested
     @DisplayName("Getters")
@@ -161,6 +114,82 @@ public class ChunkedListTest {
                 ChunkedList<String> stringChunkedList = new ChunkedList<>();
                 assertThrows(NoSuchElementException.class, stringChunkedList::getFirst);
             }
+        }
+        
+        @Nested
+        @DisplayName("Get from index")
+        class GetIndexTests {
+            @Test
+            void value_test_after_getting_from_index() {
+                final String EXPECTED = "EXPECTED VALUE";
+                ChunkedList<String> stringChunkedList = new ChunkedList<>();
+                stringChunkedList.add(EXPECTED);
+                assertEquals(EXPECTED, stringChunkedList.get(0));
+            }
+            
+            @Test
+            void value_test_out_of_bounds() {
+                final int INDEX = 999;
+                ChunkedList<String> stringChunkedList = new ChunkedList<>();
+                assertThrows(IndexOutOfBoundsException.class, () -> stringChunkedList.get(INDEX));
+            }
+            
+            @Test
+            void value_test_out_of_bounds_edge() {
+                final int INDEX = 0;
+                ChunkedList<String> stringChunkedList = new ChunkedList<>();
+                assertThrows(IndexOutOfBoundsException.class, () -> stringChunkedList.get(INDEX));
+            }
+            
+            @Test
+            void value_test_out_of_bounds_negative() {
+                final int INDEX = -10;
+                ChunkedList<String> stringChunkedList = new ChunkedList<>();
+                assertThrows(IndexOutOfBoundsException.class, () -> stringChunkedList.get(INDEX));
+            }
+        }
+    }
+    
+    @Nested
+    @DisplayName("Removing")
+    class RemovingTests {
+        @Test
+        void remove_returns_element() {
+            String valueRemoved = "Removed Element";
+            
+            ChunkedList<String> integerChunkedList = new ChunkedList<>();
+            integerChunkedList.add(valueRemoved);
+            String removed = integerChunkedList.remove(0);
+            
+            assertEquals(valueRemoved, removed);
+        }
+        
+        @Test
+        void remove_shifts_elements_left() {
+            ChunkedList<Integer> integerChunkedList = new ChunkedList<>();
+            
+            integerChunkedList.add(1);
+            integerChunkedList.add(2);
+            integerChunkedList.add(3);
+            
+            integerChunkedList.remove(1);
+            
+            assertEquals(2, integerChunkedList.length());
+            assertEquals(3, integerChunkedList.get(1));
+        }
+        
+        @Test
+        void remove_element_from_middle_chunk() {
+            ChunkedList<Integer> integerChunkedList = new ChunkedList<>();
+            
+            for (int i = 0; i < 20; i++) {
+                integerChunkedList.add(i);
+            }
+            
+            integerChunkedList.remove(10);
+            
+            assertEquals(19, integerChunkedList.length());
+            assertEquals(11, integerChunkedList.get(10));
         }
     }
 }
