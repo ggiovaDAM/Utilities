0b403450a8bfc7a4f40d48ff5093182b55d87258
ggiovadam <guido.giovadam@gmail.com>
2026-01-27 01:08:11 +0100

    [feat] Add timeNanos for quick operations

--- src/main/java/com/ggiova/utilities/time/Timing.java
+++ src/main/java/com/ggiova/utilities/time/Timing.java
@@ -16,7 +16,7 @@ import java.util.function.Supplier;
  * double result = Timing.timeMillis("Square root", () -> Math.sqrt(100));
  *
  * // Timing a Runnable with no return value
- * Timing.timeMillis("Print operation", () -> System.out.println("Hello, world!"));
+ * Timing.timeNanos("Print operation", () -> System.out.println("Hello, world!"));
  * }</pre>
  */
 public final class Timing {
@@ -125,6 +125,109 @@ public final class Timing {
      */
     public static void timeMillis(Runnable action) {
         timeMillis(() -> {
+            action.run();
+            return null;
+        });
+    }
+    
+    /**
+     * Times the execution of the given function. Prints the time taken in milliseconds. Returns the result of the
+     * operation.
+     *
+     * <p>Example code:
+     * <pre>{@code
+     * double result = Timing.timeNanos(
+     *         "SQRT",
+     *         () -> Math.sqrt(100)
+     * );
+     * }</pre>
+     * printing:
+     * <pre>{@code SQRT took 15800 ns}</pre>
+     *
+     * @param label  Name given to the operation being performed
+     * @param action Action to be measured
+     * @param <T>    Result of the operation
+     * @return The result of running the operation
+     */
+    @SuppressWarnings("UseOfSystemOutOrSystemErr")
+    public static <T> T timeNanos(String label, Supplier<T> action) {
+        long start = System.nanoTime();
+        T result = action.get();
+        long end = System.nanoTime();
+        System.out.printf("%s took %d ns%n", label, end - start);
+        return result;
+    }
+    
+    /**
+     * Times the execution of the given function. Prints the time taken in milliseconds. Returns the result of the
+     * operation.
+     * <p>Example code:
+     * <pre>{@code
+     * double result = Timing.timeMillis(
+     *     () -> Math.sqrt(100)
+     * );
+     * }</pre>
+     * printing:
+     * <pre>{@code Took 26500 ns}</pre>
+     *
+     * @param action Action to be measured
+     * @param <T>    Result of the operation
+     * @return The result of running the operation
+     */
+    @SuppressWarnings("UseOfSystemOutOrSystemErr")
+    public static <T> T timeNanos(Supplier<T> action) {
+        long start = System.nanoTime();
+        T result = action.get();
+        long end = System.nanoTime();
+        System.out.printf("Took %d ns%n", end - start);
+        return result;
+    }
+    
+    /**
+     * Times the execution of the given function. Prints the time taken in milliseconds. Does not return anything.
+     *
+     * <p>Example code:
+     * <pre>{@code
+     * Timing.timeMillis(
+     *     "Print operation",
+     *     () -> System.out.println("Hello, world!")
+     * );
+     * }</pre>
+     * printing:
+     * <pre>{@code
+     * Hello, world!
+     * Print operation took 227200 ns
+     * }</pre>
+     *
+     * @param label  Name given to the operation being performed
+     * @param action Action to be measured
+     */
+    public static void timeNanos(String label, Runnable action) {
+        timeNanos(label, () -> {
+            action.run();
+            return null;
+        });
+    }
+    
+    /**
+     * Times the execution of the given function. Prints the time taken in milliseconds. Does not return anything.
+     *
+     * <p>Example code:
+     * <pre>{@code
+     * Timing.timeMillis(
+     *         () -> System.out.println("Hello, world!")
+     * );
+     * }</pre>
+     * printing:
+     * <pre>{@code
+     * Hello, world!
+     * Took 186000 ns
+     * }</pre>
+     *
+     * @param action Action to be measured
+     */
+    public static void timeNanos(Runnable action) {
+        timeNanos(() -> {
             action.run();
             return null;
         });
