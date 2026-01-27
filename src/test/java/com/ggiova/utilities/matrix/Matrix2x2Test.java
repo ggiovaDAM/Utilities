f4b46fe138334d4b386e7944af836d2b12d6d208
ggiovadam <guido.giovadam@gmail.com>
2026-01-28 00:22:07 +0100

    [feat] 2x2 matrix tests skeleton

---
+++ src/test/java/com/ggiova/utilities/matrix/Matrix2x2Test.java
@@ -0,0 +1,45 @@
+package com.ggiova.utilities.matrix;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
+
+/**
+ * Test class for {@link Matrix2x2}.
+ */
+public class Matrix2x2Test {
+    @Nested
+    @DisplayName("Constructor Tests")
+    class ConstructorTests {
+    
+    }
+    
+    @Nested
+    @DisplayName("Get and Set Tests")
+    class GetSetTests {
+    
+    }
+    
+    @Nested
+    @DisplayName("Conversion Tests")
+    class ConversionTests {
+    
+    }
+    
+    @Nested
+    @DisplayName("Equals and HashCode Tests")
+    class EqualsHashCodeTests {
+    
+    }
+    
+    @Nested
+    @DisplayName("ToString Tests")
+    class ToStringTests {
+    
+    }
+    
+    @Nested
+    @DisplayName("Edge Cases")
+    class EdgeCaseTests {
+    
+    }
+}
