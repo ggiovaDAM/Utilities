70b4a8c83ecf4bde3252e3a8d8a36238071f9e2b
ggiovadam <guido.giovadam@gmail.com>
2026-01-27 02:01:05 +0100

    [feat] add BigIntegers for constants and utilities

---
+++ src/main/java/com/ggiova/utilities/primitives/BigIntegers.java
@@ -0,0 +1,8 @@
+package com.ggiova.utilities.primitives;
+
+import java.math.BigInteger;
+
+public final class BigIntegers {
+    public static final BigInteger NEGATIVE_ONE = BigInteger.valueOf(-1L);
+    public static final BigInteger NEGATIVE_TWO = BigInteger.valueOf(-2L);
+}
