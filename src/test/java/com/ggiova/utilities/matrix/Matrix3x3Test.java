2f24f4fdd7245903bc6bbb03067f4b943f2551ea
ggiovadam <guido.giovadam@gmail.com>
2026-01-28 00:23:03 +0100

    [feat] 3x3 matrix tests skeleton

---
+++ src/test/java/com/ggiova/utilities/matrix/Matrix3x3Test.java
@@ -0,0 +1,45 @@
+package com.ggiova.utilities.matrix;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
+
+/**
+ * Test class for {@link Matrix3x3}.
+ */
+public class Matrix3x3Test {
+    @Nested
+    @DisplayName("Constructor Tests")
+    class ConstructorTests {
+    
+    }
+    
+    @Nested
+    @DisplayName("Get and Set Tests")
+    class GetSetTests {
+    
+    }
+    
+    @Nested
+    @DisplayName("Conversion Tests")
+    class ConversionTests {
+    
+    }
+    
+    @Nested
+    @DisplayName("Equals and HashCode Tests")
+    class EqualsHashCodeTests {
+    
+    }
+    
+    @Nested
+    @DisplayName("ToString Tests")
+    class ToStringTests {
+    
+    }
+    
+    @Nested
+    @DisplayName("Edge Cases")
+    class EdgeCaseTests {
+    
+    }
+}
